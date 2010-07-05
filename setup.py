07b0d7f70a5763c5993925ffd0a9150aee01051e
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-07-05 00:35:51 -0700

    Tiger 0.2.

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ tigersources = ['sboxes.c', 'tiger.c', 'tigermodule.c']
 
 tiger = Extension("tiger", sources = tigersources, extra_compile_args=['-g3'])
 
-setup(name = "tiger", version = "0.1", description = "Tiger hash module",
+setup(name = "tiger", version = "0.2", description = "Tiger hash module",
 	ext_modules = [tiger])
